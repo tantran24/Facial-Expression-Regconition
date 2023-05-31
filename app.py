51976c499b4d376cea41601f8a0b442b4b91c37a
Trần Duy Tân <tranduytan24@gmail.com>
2023-05-31 10:55:00 +0700

    up

--- app.py
+++ app.py
@@ -39,28 +39,36 @@ async def dynamic(request: Request, photo: UploadFile = File()):
     # encoding the image
     nparr = np.frombuffer(img, np.uint8)
     color_img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
-
+    # width, height, _ = color_img.shape
     gray_img = cv2.imdecode(nparr, cv2.IMREAD_GRAYSCALE)
     img = cv2.cvtColor(gray_img,cv2.COLOR_GRAY2RGB)
-
+    
     face_locations = face_recognition.face_locations(img)
-
+    print('So luong khuon mat:', len(face_locations))
     for locatation in face_locations:
         top, right, bottom, left = locatation
         cv2.rectangle(color_img, (left, top), (right, bottom), (0, 255, 0), 4)
         face = img[top:bottom, left:right]
-
+        
         #predict
         inputs = feature_extractor(face, return_tensors="pt")
 
         with torch.no_grad():
             logits = model(**inputs).logits
-        
+            softmax_vals = torch.softmax(logits, dim=-1)
         predicted_label = logits.argmax(-1).item()
-        result =  emotions[predicted_label]
+        confidence = softmax_vals[0][predicted_label]
 
+        result =  emotions[predicted_label]
+        print(result)
+        
         #put labels
-        cv2.putText(color_img, result, (left, top-5), font, 0.8, text_color, 1, cv2.LINE_AA)
+        (w, h), _ = cv2.getTextSize(result, cv2.FONT_HERSHEY_SIMPLEX, 0.4, 1)
+        # font_scale = min(width / w, height/ h) 
+        img = cv2.rectangle(color_img, (left, bottom), (left + w, bottom + 3*h), (0, 255, 0), -1)
+        img = cv2.putText(color_img, result, (left, bottom + h), cv2.FONT_HERSHEY_SIMPLEX, 0.4, text_color, 1)
+        img = cv2.putText(color_img, str(round(float(confidence),2)), (left, bottom + 2*h + h//2), cv2.FONT_HERSHEY_SIMPLEX, 0.4, text_color, 1)
+
 
 
     buffer = cv2.imencode('.jpg', color_img)[1]
@@ -79,7 +87,7 @@ async def dynamic(request: Request, photo: UploadFile = File()):
 
     encoded_image = base64.b64encode(buffer).decode('utf-8')
 
-    return templates.TemplateResponse("web.html", {"request": request, "photo": encoded_image, "result": result})
+    return templates.TemplateResponse("web.html", {"request": request, "photo": encoded_image})
 
 
 
